4ff91f066564ff0a5cc7dfea46f05041f39feef3
aksah07 <aksahroseshaji@gmail.com>
2026-02-06 10:27:33 +0530

    Fixed mongo config import

--- Backend/db/mongo.py
+++ Backend/db/mongo.py
@@ -1,10 +1,12 @@
 from pymongo import MongoClient
-from config import MONGO_URI, DB_NAME
+from config import Config
 
-client = MongoClient(MONGO_URI)
+# Initialize Mongo client using Config class
+client = MongoClient(Config.MONGO_URI)
 
-db = client[DB_NAME]
+db = client[Config.DB_NAME]
 
+# Collections
 ngos_collection = db["ngos"]
 shelters_collection = db["shelters"]
 requests_collection = db["requests"]
